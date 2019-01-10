4cf4c6a617a16d8a373598c28684fd68c1089dc9
Eric Seamons <eseamons2@mmm.com>
2019-01-10 14:27:14 -0700

    use jquery to adjust position of location header according to image height

--- src/app/location/location.component.ts
+++ src/app/location/location.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-location',
@@ -10,6 +11,19 @@ export class LocationComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+	  
+	$(document).ready(function() {
+		let $image = $(".responsive-image")
+		$image.on('load', function(){
+		  let $marginTop = $(this).height() - 100
+		  $('.page-header-text').css({ 'margin-top': $marginTop}); 
+		});
+
+		$(window).resize(function(){
+			let $marginTop = $image.height() - 100
+			$('.page-header-text').css({ 'margin-top': $marginTop}); 
+		});
+	});
   }
 
 }
