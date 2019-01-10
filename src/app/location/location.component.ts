4f813b67f6dcdca201742a1ce41fb7080fea8670
Eric Seamons <eseamons2@mmm.com>
2019-01-10 10:57:20 -0700

    added convention information to location page

--- src/app/location/location.component.ts
+++ src/app/location/location.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-location',
   templateUrl: './location.component.html',
-  styleUrls: ['./location.component.css']
+  styleUrls: ['./location.component.css', '../bootstrap.min.css']
 })
 export class LocationComponent implements OnInit {
 
