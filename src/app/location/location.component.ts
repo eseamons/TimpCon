4b806abd6ada0f3ed5a6d1b72af165106faa046a
Eric Seamons <eseamons2@mmm.com>
2019-01-10 15:00:18 -0700

    now code to set page header label is in navbar component typescript file. Now all components have a label.

--- src/app/location/location.component.ts
+++ src/app/location/location.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import * as $ from 'jquery';
 
 @Component({
   selector: 'app-location',
   templateUrl: './location.component.html',
-  styleUrls: ['./location.component.css', '../bootstrap.min.css']
+  styleUrls: ['./location.component.css', '../bootstrap.min.css'],
+  
 })
 export class LocationComponent implements OnInit {
 
@@ -12,18 +12,6 @@ export class LocationComponent implements OnInit {
 
   ngOnInit() {
 	  
-	$(document).ready(function() {
-		let $image = $(".responsive-image")
-		$image.on('load', function(){
-		  let $marginTop = $(this).height() - 100
-		  $('.page-header-text').css({ 'margin-top': $marginTop}); 
-		});
-
-		$(window).resize(function(){
-			let $marginTop = $image.height() - 100
-			$('.page-header-text').css({ 'margin-top': $marginTop}); 
-		});
-	});
   }
 
 }
