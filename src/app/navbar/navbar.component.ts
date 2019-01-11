af69022af20341ef62103a4567410072467ee3f2
Eric Seamons <eseamons2@mmm.com>
2019-01-11 08:20:20 -0700

    page header label now is positioned correctly in Microsoft Edge

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -10,14 +10,23 @@ export class NavbarComponent implements OnInit {
 
   constructor() { }
 
+
   ngOnInit() {
-	  	  
+
 	$(document).ready(function() {
 		let $image = $(".responsive-image")
 		$image.on('load', function(){
 		  let $marginTop = $(this).height() - 100
 		  $('.page-header-text').css({ 'margin-top': $marginTop}); 
 		});
+		
+		setTimeout(function() { //This is necessary because there is trouble with loading ie and Microsoft Edge
+			if (/Edge/.test(navigator.userAgent)) {
+				let $marginTop = 	$image.height() - 100
+				$('.page-header-text').css({ 'margin-top': $marginTop}); 
+			}
+		  
+		}, 0);
 
 		$(window).resize(function(){
 			let $marginTop = $image.height() - 100
