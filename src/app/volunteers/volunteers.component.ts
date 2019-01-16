235321cb6c52bd6f2df3bd22c0a823ed4ce8bee5
Eric Seamons <eseamons2@mmm.com>
2019-01-16 13:04:52 -0700

    Added and formatted 'Apply' button in volunteer component

--- src/app/volunteers/volunteers.component.ts
+++ src/app/volunteers/volunteers.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-volunteers',
   templateUrl: './volunteers.component.html',
-  styleUrls: ['./volunteers.component.css']
+  styleUrls: ['./volunteers.component.css', '../bootstrap.min.css']
 })
 export class VolunteersComponent implements OnInit {
 
