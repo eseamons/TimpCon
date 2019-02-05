aa8095c755aa5eceb6c21a36b3ac415971fb86f8
Eric Seamons <eseamons2@mmm.com>
2019-02-05 15:36:14 -0700

    Height of video now adjusts, however centering of the video still has issues. I did a little hack to make it as close as possible, but I will need to come up with a more permenant solution.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-home',
@@ -9,7 +10,37 @@ export class HomeComponent implements OnInit {
 
   constructor() { }
 
+
   ngOnInit() {
+	
+	$(document).ready(function() {
+		let $image = $(".responsive-image")
+		let $video = $('.timpcon-video')
+		let $video_wrapper = $('.timp-video-wrapper')
+		
+		$image.on('load', function(){
+		  let $height = $(this).height() - 100
+		  let $width = $(this).width()
+		  $video.css({'height': $height});
+
+		});
+		
+		setTimeout(function() { //This is necessary because there is trouble with loading ie and Microsoft Edge
+			if (/Edge/.test(navigator.userAgent)) {
+				let $height = $image.height() - 100
+				let $width = $(this).width()
+				$video.css({'height': $height});
+			}
+		  
+		}, 0);
+
+		$(window).resize(function(){
+			let $height = $image.height() - 100
+			let $width = $(this).width()
+			$video.css({'height': $height});
+		});
+	});
+	  
   }
 
 }
